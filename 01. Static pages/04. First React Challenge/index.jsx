93a958f738a3e3bff0a70dbc198e92d8ba289fe7
rfencl <rick_fencl@hotmail.com>
2025-08-26 10:18:55 -0700

    4: update

--- 01. Static pages/04. First React Challenge/index.jsx
+++ 01. Static pages/04. First React Challenge/index.jsx
@@ -1,5 +1,18 @@
-/**
- * Challenge: Set up a React app from scratch again.
- * This time, try rendering an unordered list with 2-3 list items inside
- * with why you're excited to be learning React.
- */
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <main>
+        <img width="40px"
+            src="images/react-logo.png"
+            alt="React logo"
+        />
+        <h1>Fun facts about React</h1>
+        <ul class="facts-list">
+            <li >Was first released in 2013</li>
+            <li>Was originally created by XXXXXX Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul    >
+    </main>
+)   
+
